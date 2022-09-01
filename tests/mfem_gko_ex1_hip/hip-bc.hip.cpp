eb48f094ce6b4d3b399a442d7e989259907abc69
nbeams <246972+nbeams@users.noreply.github.com>
2022-09-01 16:56:44 -0400

    Minor fixes to HIP version of MFEM-Ginkgo test

--- tests/mfem_gko_ex1_hip/hip-bc.hip.cpp
+++ tests/mfem_gko_ex1_hip/hip-bc.hip.cpp
@@ -3,7 +3,7 @@
 // Minimal recreation of MFEM's "FORALL" wrapper as used for ConstrainedOperators: kernel,
 // wrapper for calling kernel, function that chooses device or host.
 template<typename T>
-__launch_bounds__(256) __global__ static void HipSetConstraints(const int N, const int* constraint_list, T* d_data)
+__launch_bounds__(256) __global__ static void HipSetConstraintsKernel(const int N, const int* constraint_list, T* d_data)
 {
    const int k = blockDim.x*blockIdx.x + threadIdx.x;
    if (k >= N) { return; }
@@ -15,13 +15,13 @@ void HipWrapConstraintSet(const int N, const int* constraint_list, T *d_data)
 {
    if (N==0) { return; }
    const int GRID = (N+BLCK-1)/BLCK;
-   HipSetConstraints<<<GRID,BLCK>>>(N, constraint_list, d_data);
+   HipSetConstraintsKernel<<<GRID,BLCK>>>(N, constraint_list, d_data);
 }
 template void HipWrapConstraintSet(const int N, const int* constraint_list, double *d_data);
 template void HipWrapConstraintSet(const int N, const int* constraint_list, float *d_data);
 
 template<typename T>
-__launch_bounds__(256) __global__ static void HipTransferMeshCoords(const int N, const double* mfem_coords, T* coords)
+__launch_bounds__(256) __global__ static void HipTransferMeshCoordsKernel(const int N, const double* mfem_coords, T* coords)
 {
    const int k = blockDim.x*blockIdx.x + threadIdx.x;
    if (k >= N) { return; }
@@ -33,7 +33,7 @@ void HipTransferMeshCoords(const int N, const double* mfem_coords, T* coords)
 {
    if (N==0) { return; }
    const int GRID = (N+BLCK-1)/BLCK;
-   HipTransferMeshCoords<T><<<GRID,BLCK>>>(N, mfem_coords, coords);
+   HipTransferMeshCoordsKernel<T><<<GRID,BLCK>>>(N, mfem_coords, coords);
 }
 
 template void HipTransferMeshCoords<double>(const int N, const double* mfem_coords, double* coords);
